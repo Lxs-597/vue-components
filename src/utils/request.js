22ba517761db316fdd27200535710f2b99aef804
Lxs-597 <312181653@qq.com>
2018-07-09 11:06:34 +0800

    axios超时处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,49 +1,100 @@
 import axios from 'axios'
 import Qs from 'qs'
 
+// import { getToken } from 'utils/catch'
+
 const service = axios.create({
-  baseURL: 'http://localhost:9000',
+  baseURL: '',
   timeout: 10000,
-  headers: {
-    'X-Requested-With': 'XMLHttpRequest'
-  }
+  'X-Requested-With': 'XMLHttpRequest'
 })
 
+service.defaults.retry = 3
+service.defaults.retryDelay = 1000
 service.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
 
 service.interceptors.request.use(config => {
-  const token = '' // 获取token
+  const token = getToken()
 
   if (token) {
-    config.headers.common['Authorization'] = token
+    config.headers['Authorization'] = token
   }
 
   // loading
-
   return config
 }, error => {
-  return Promise.reject(error)
+  console.log('request error', error)
+  return Promise.reject(error.message)
 })
 
 service.interceptors.response.use(response => {
+
   return response
 }, error => {
-  return Promise.resolve(error.response)
+  const config = error.config
+
+  if (!config || !config.retry) {
+    return Promise.reject(error.message)
+  }
+
+  console.log(error)
+
+  config.__retryCount = config.__retryCount || 0
+
+  if (config.__retryCount >= config.retry) {
+    if (error && error.response) {
+      switch (error.response.status) {
+        case 400:
+          return Promise.reject('请求错误!')
+        case 401:
+          return Promise.reject('权限错误!')
+        case 403:
+          return Promise.reject('拒绝访问!')
+        case 404:
+          return Promise.reject('请求失败!')
+        case 408:
+          return Promise.reject('请求超时!')
+        case 500:
+          return Promise.reject('服务器错误!')
+        default:
+          return Promise.reject('未知错误!')
+      }
+    }
+
+    if (error.code == 'ECONNABORTED') {
+      return Promise.reject('请求超时!')
+    }
+
+    return Promise.reject(error.message)
+  }
+
+  config.__retryCount += 1
+
+  const backOff = new Promise(resolve => {
+    setTimeout(resolve, config.retryDelay || 1000)
+  })
+  
+  return backOff.then(() => service(config))
 })
 
 export default {
-  get(url, params) {
-    return service({
-      method: 'get',
-      url,
-      params
-    })
-  },
-  post(url, data) {
+  post (url, data, conifg={}) {
     return service({
       method: 'post',
       url,
-      data: Qs.stringify(data)
-    })
+      data: Qs.stringify(data),
+      conifg
+    }).then(
+      res => Promise.resolve(res.data)
+    )
+  },
+  get (url, params) {
+    return service({
+      method: 'get',
+      url,
+      params,
+    }).then(
+      res => Promise.resolve(res.data)
+    )
   }
 }
