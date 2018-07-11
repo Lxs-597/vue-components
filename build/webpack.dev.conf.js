a8d3e601ea890e4e0988bfa9535f8af4ef7159dc
Lxs-597 <312181653@qq.com>
2018-07-11 23:25:15 +0800

    remove logo

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -56,7 +56,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
       filename: 'index.html',
       template: 'index.html',
       inject: true,
-      favicon: './logo.png'
+      // favicon: './logo.png'
     }),
     // copy custom static assets
     new CopyWebpackPlugin([
